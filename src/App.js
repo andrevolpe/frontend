4cebc0fb5c2d2b32b0a4342148e2ebfad31426b9
andrevolpe <andrevolpe@hotmail.com.br>
2020-05-20 01:06:03 +0000

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { XMLNS_1_0 } from 'xmlchars';
 
+const lista = [
+    { id: 1, name: 'Estudar', done: false},
+        { id: 1, name: 'Estudar', done: true},
+            { id: 1, name: 'Estudar', done: true}
+]
+ 
 function App() {
-  return <h1>ReactJS, Template!</h1>
+  return (
+      <table> 
+          {lista.map(item =>(    
+              <tr key={item.id}>
+              <td> {item.id} </td>
+              <td> {item.name} </td>
+              <td> <input type="checkbox" checked={item.done}/> </td>
+          </tr>       
+          ))}
+      </table>
+  );
 }
 
 export default App;
